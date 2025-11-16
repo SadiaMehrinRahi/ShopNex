da1bea1709f6223264808741cf8e8bb674701e15
Sanjida Khanom <SKtinker>
2025-11-17 02:46:51 +0600

    Invoice API add

---
+++ src/controllers/InvoiceController.js
@@ -0,0 +1,27 @@
+exports.CreateInvoice = (req, res) => {
+    res.send("CreateInvoice endpoint not implemented.");
+};
+
+exports.InvoiceList = (req, res) => {
+    res.send("InvoiceList endpoint not implemented.");
+};
+
+exports.InvoiceProductList = (req, res) => {
+    res.send("InvoiceProductList endpoint not implemented.");
+};
+
+exports.PaymentSuccess = (req, res) => {
+    res.send("PaymentSuccess endpoint not implemented.");
+};
+
+exports.PaymentCancel = (req, res) => {
+    res.send("PaymentCancel endpoint not implemented.");
+};
+
+exports.PaymentFail = (req, res) => {
+    res.send("PaymentFail endpoint not implemented.");
+};
+
+exports.PaymentIPN = (req, res) => {
+    res.send("PaymentIPN endpoint not implemented.");
+};
